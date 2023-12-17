0489ccbda47ea031eba86e3b46cbded7656ca05b
Timur <timur.minullin535@gmail.com>
2023-12-17 19:28:30 +0300

    add views and models details

--- Game/ViewModels/MainWindowViewModel.cs
+++ Game/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,45 @@
-﻿namespace Game.ViewModels;
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reactive;
+using System.Threading.Tasks;
+using Game.Models;
+using ReactiveUI;
+
+namespace Game.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
+    public MainWindowViewModel()
+    {
+        Initialize();
+        Player = new Player();
+        ConnectCommand = ReactiveCommand.Create(Connect);
+    }
+
+    private void Initialize()
+    {
+        var files = Directory.GetFiles("../../../Assets/Rules/");   
+        var images = files.Select(x => new Rules(new FileInfo(x).FullName));
+        RuleImages = new ObservableCollection<Rules>(images);
+    }
+
     public string Greeting => "Welcome to Cow 006!";
-#pragma warning restore CA1822 // Mark members as static
+ 
+    public Player Player { get; }
+    
+    private ObservableCollection<Rules>? _ruleImages;
+    
+    public ObservableCollection<Rules>? RuleImages
+    {
+        get => _ruleImages;
+        set
+        {
+            if (value != null) _ruleImages = value;
+        }
+    }
+
+    public ReactiveCommand<Unit, Unit> ConnectCommand { get; }
+    
+    private void Connect() => Task.Run(() => Player.ConnectAsync());
 }
--- Game/Views/MainWindow.axaml.cs
+++ Game/Views/MainWindow.axaml.cs
@@ -13,5 +13,31 @@ public partial class MainWindow : Window
     private void OpenMainMenu_OnClick(object? sender, RoutedEventArgs e)
     {
         GreetingPage.IsVisible = false;
+        MainMenuPage.IsVisible = true;
+        RulesPage.IsVisible = false;
+    }
+
+    private void StartButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        MainMenuPage.IsVisible = false;
+        NickInput.IsVisible = true;
+    }
+
+    private void Rules_OnClick(object? sender, RoutedEventArgs e)
+    {
+        GreetingPage.IsVisible = false;
+        MainMenuPage.IsVisible = false;
+        RulesPage.IsVisible = true;
+    }
+
+    private void CloseWindow_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void PlayerJoin_OnClick(object? sender, RoutedEventArgs e)
+    {
+        NickInput.IsVisible = false;
+        GameGrid.IsVisible = true;
     }
 }
